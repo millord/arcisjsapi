ada1165b0961633ea8b54404e19834e5f03bbfab
millord <juancarlosmillord@gmail.com>
2020-03-12 07:08:59 -0400

    first commit

---
+++ 3d/app.js
@@ -0,0 +1,20 @@
+require(["esri/Map", "esri/views/SceneView"], function(Map, SceneView) {
+  var map = new Map({
+    basemap: "satellite",
+    ground: "world-elevation" // show elevation
+  });
+
+  var view = new SceneView({
+    container: "viewDiv",
+    map: map,
+    camera: {
+      position: {
+        // observation point
+        x: -118.808,
+        y: 33.961,
+        z: 25000 // altitude in meters
+      },
+      tilt: 65 // perspective in degrees
+    }
+  });
+});
